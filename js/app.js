854e3e6c05581f895a10fd4d3f8923d15ea5c99c
zzttwzq <1076976262@qq.com>
2017-11-18 15:16:17 +0800

    1212

--- js/app.js
+++ js/app.js
@@ -13,5 +13,23 @@ var app = angular.module("app",[
     controller:'indexlist',
     cache: false,
   })
+  .state('project',{
+    url: '/project',
+    templateUrl: "/progman/tpls/myproject.php",
+    controller:'project',
+    cache: false,
+  })
+  .state('addproject',{
+    url: '/addproject',
+    templateUrl: "/progman/tpls/addproject.php",
+    controller:'addproject',
+    cache: false,
+  })
+  .state('note',{
+    url: '/note',
+    templateUrl: "/progman/tpls/note.php",
+    controller:'note',
+    cache: false,
+  })
 
 });
--- js/ngcontroller/controller.js
+++ js/ngcontroller/controller.js
@@ -1,15 +1,19 @@
 var controllers = angular.module("controller",[
-  "factory"
+  "factory",
+  "ui.router"
 ])
-.controller("navview",function($scope){
+.controller("navview",function($scope,$state){
+
+  $scope.list = [{name:"学习记录",img:"glyphicon glyphicon-home cellimg",active:"true",page:0,url:"index"},
+                {name:"我的项目",img:"glyphicon glyphicon-book cellimg",active:"false",page:1,url:"project"},
+                {name:"添加项目",img:"glyphicon glyphicon-plus-sign cellimg",active:"false",page:2,url:"addproject"},
+                {name:"随身笔记",img:"glyphicon glyphicon-edit cellimg",active:"false",page:3,url:"note"}];
 
 
+  //默认进入首页
+  $state.go("index");
 
-  $scope.list = [{name:"学习记录",img:"glyphicon glyphicon-home cellimg",active:"true",page:0},
-                {name:"我的项目",img:"glyphicon glyphicon-book cellimg",active:"false",page:1},
-                {name:"添加项目",img:"glyphicon glyphicon-plus-sign cellimg",active:"false",page:2},
-                {name:"随身笔记",img:"glyphicon glyphicon-edit cellimg",active:"false",page:3}];
-
+  //按钮点击 添加选中的颜色
   var lastclick = 0;
   $scope.click = function(item){
     item.active = true;
@@ -19,19 +23,18 @@ var controllers = angular.module("controller",[
 
     lastclick = item.page;
     $scope.list = $scope.list;
+
+    //进入页面
+    $state.go(item.url);
   }
 
-  var inint = function (){
-    
-    $scope.list = $scope.list;
-  }
-  inint();
-
+  //经过按钮 添加选中的颜色
   $scope.over = function(item){
     item.active = true;
     $scope.list = $scope.list;
   }
 
+  //离开按钮 去掉选中的颜色
   $scope.leave = function(item){
     item.active = false;
 
@@ -40,9 +43,23 @@ var controllers = angular.module("controller",[
 
     $scope.list = $scope.list;
   }
-
 })
 .controller("indexlist",function($scope){
+
+
+
+})
+.controller("project",function($scope){
+
+
+
+})
+.controller("addproject",function($scope){
+
+
+
+})
+.controller("note",function($scope){
 
 
 
